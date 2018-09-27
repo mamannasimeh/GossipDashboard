c2dfd25ce98cbb85b8910a56bc91a1d6e8bbe414
m.kabi <mahmoodkabi@gmail.com>
2018-09-27 11:40:15 +0330

    ایجاد کلاس برای مدیریت پست ها

--- GossipDashboard/Controllers/HomeController.cs
+++ GossipDashboard/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using GossipDashboard.Models;
 using System.IO;
 using System.Text;
+using GossipDashboard.Helper;
 
 namespace GossipDashboard.Controllers
 {
@@ -20,38 +21,24 @@ namespace GossipDashboard.Controllers
         {
             //string path = ControllerContext.HttpContext.Server.MapPath("~/Templates/category-quiz.html");
             path = ControllerContext.HttpContext.Server.MapPath("~");
-
-            var docTemplates = new HtmlDocument();
-            docTemplates.Load(path + "/Templates/category-quiz.html", System.Text.Encoding.UTF8);
-
-            //HtmlDocument copyDocTemplates = new HtmlDocument();
-            //copyDocTemplates.DocumentNode.CopyFrom(docTemplates.DocumentNode);
+            PostManagement postManagement = new PostManagement(path);
 
             var docIndex = new HtmlDocument();
             docIndex.Load(path + "/Views/Home/Index.cshtml", System.Text.Encoding.UTF8);
             var nodesIndex = docIndex.DocumentNode.SelectNodes("//div");
 
             //حذف محتويات ند بلاك-author-grid
-            foreach (var itemNode in nodesIndex)
-            {
-                var attrs = itemNode.Attributes;
-                foreach (var itemAttr in attrs)
-                {
-                    if (itemAttr.Value == "author-grid")
-                    {
-                        itemNode.RemoveAllChildren();
-                        break;
-                    }
-                }
-            }
+            postManagement.ClearContentNode(nodesIndex, "author-grid");
 
+            //ایجاد  تگ آرتیکل به ازای هر پست
             var postQuiz = context.Posts.ToList();
             foreach (var item in postQuiz)
             {
-                var itSelfNode = CreateHead(item);
+                //ايجاد محتوا براي وسط صفحه-- author-grid 
+                var itSelfNode = postManagement.CreateHead(item, "/Templates/category-quiz.html");
                 if (itSelfNode != null)
                 {
-                    result = AddHeadToContent(nodesIndex, itSelfNode);
+                    result = postManagement.AddHeadToContent(nodesIndex, "author-grid", itSelfNode);
                 }
             }
 
@@ -63,63 +50,6 @@ namespace GossipDashboard.Controllers
         }
 
 
-        //ايجاد محتوا براي وسط صفحه-- author-grid 
-        private HtmlNode CreateHead(Post post)
-        {
-            var docTemplates = new HtmlDocument();
-            docTemplates.Load(path + "/Templates/category-quiz.html", System.Text.Encoding.UTF8);
-
-            var nodes = docTemplates.DocumentNode.SelectNodes("//div");
-            foreach (var itemNode in nodes)
-            {
-                var attrs = itemNode.Attributes;
-                foreach (var itemAttr in attrs)
-                {
-                    //entry-cover پيدا كردن تگ ديو با كلاس 
-                    if (itemAttr.Value == "entry-cover")
-                    {
-                        //ايجاد entry-cover
-                        HtmlNode oldChild = itemNode.SelectSingleNode("//a");//FirstChild;
-                        HtmlNode newChild = HtmlNode.CreateNode("<a href='" + post.Url + "' name='" + post.PostID + "'>  <img width='290' height='170' src='" + post.Image1 + "'  alt='" + post.Subject + "' />  </a>");
-                        itemNode.ReplaceChild(newChild, oldChild);
-
-                        //برادر بعدي
-                        //ايجاد entry-content
-
-
-                        // برادر بعدي
-                        //ايجاد entry-footer
-
-
-                        //article ايجاد تگ 
-                        HtmlNode articleNode = HtmlNode.CreateNode("<article class='col-md-4 format-standard hentry category-quiz'></article>");
-                        articleNode.AppendChild(nodes.FirstOrDefault());
-                        return articleNode;
-                    }
-                }
-            }
-
-            return null;
-        }
-
-        //اضافه كردن محتوا به وسط صفحه -- author-grid 
-        private HtmlNode AddHeadToContent(HtmlNodeCollection nodesIndex, HtmlNode itSelfNode)
-        {
-            foreach (var itemNode in nodesIndex)
-            {
-                var attrs = itemNode.Attributes;
-                foreach (var itemAttr in attrs)
-                {
-                    if (itemAttr.Value == "author-grid")
-                    {
-                        itemNode.AppendChild(itSelfNode);
-                        return nodesIndex.FirstOrDefault();
-                    }
-                }
-            }
-
-            return null;
-        }
 
         public ActionResult About()
         {
@@ -132,7 +62,7 @@ namespace GossipDashboard.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
-            return View("Contact",new Object()); 
+            return View(); 
         }
     }
 
---
+++ GossipDashboard/Helper/PostManagement.cs
@@ -0,0 +1,119 @@
+﻿using GossipDashboard.Models;
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GossipDashboard.Helper
+{
+    public class PostManagement
+    {
+        private string path;
+
+        public PostManagement()
+        {
+
+        }
+
+        public PostManagement(string path)
+        {
+            this.path = path;
+        }
+
+        /// <summary>
+        /// ساخت آرتیکل های معمولی وسط صفحه
+        /// </summary>
+        /// <param name="post">شی پست</param>
+        /// <param name="templateCategory">تمپلیتی که قصد داریم از روی آن آرتیکل را بسازیم</param>
+        /// <returns></returns>
+        internal HtmlNode CreateHead(Post post, string templateCategory)
+        {
+            var docTemplates = new HtmlDocument();
+            docTemplates.Load(path + templateCategory , System.Text.Encoding.UTF8);
+
+
+            var nodes = docTemplates.DocumentNode.SelectNodes("//div");
+            foreach (var itemNode in nodes)
+            {
+                var attrs = itemNode.Attributes;
+                foreach (var itemAttr in attrs)
+                {
+                    //entry-cover پيدا كردن تگ ديو با كلاس 
+                    if (itemAttr.Value == "entry-cover")
+                    {
+                        //ايجاد entry-cover
+                        HtmlNode oldChild = itemNode.SelectSingleNode("//a");//FirstChild;
+                        HtmlNode newChild = HtmlNode.CreateNode("<a href='" + post.Url + "' name='" + post.PostID + "'>  <img width='290' height='170' src='" + post.Image1 + "'  alt='" + post.Subject + "' />  </a>");
+                        itemNode.ReplaceChild(newChild, oldChild);
+
+                        //برادر بعدي
+                        //ايجاد entry-content
+
+
+                        // برادر بعدي
+                        //ايجاد entry-footer
+
+
+                        //article ايجاد تگ 
+                        HtmlNode articleNode = HtmlNode.CreateNode("<article class='col-md-4 format-standard hentry category-quiz'></article>");
+                        articleNode.AppendChild(nodes.FirstOrDefault());
+                        return articleNode;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// اضافه كردن محتوا به وسط صفحه
+        /// </summary>
+        /// <param name="nodesIndex">ند محتوا</param>
+        /// <param name="attrValue">نام کلاسی که مشخص می کند محتوای این قسمت را می خواهیم</param>
+        /// <param name="itSelfNode">ندی که می خواهید به محتوا اضافه کنید</param>
+        /// <returns></returns>
+        internal HtmlNode AddHeadToContent(HtmlNodeCollection nodesIndex, string attrValue, HtmlNode itSelfNode)
+        {
+            foreach (var itemNode in nodesIndex)
+            {
+                var attrs = itemNode.Attributes;
+                foreach (var itemAttr in attrs)
+                {
+                    if (itemAttr.Value == attrValue)
+                    {
+                        itemNode.AppendChild(itSelfNode);
+                        return nodesIndex.FirstOrDefault();
+                    }
+                }
+            }
+
+            return null;
+        }
+
+
+        /// <summary>
+        /// پاک کردن محتوایات ند
+        /// </summary>
+        /// <param name="nodesIndex">ندی که قصد دارید تمامی محتوا یا قسمتی از محتوای آن را حذف کنید</param>
+        /// <param name="attrValue">نام کلاسی که مشخص می کند محتوا از این قسمت پاک گردد</param>
+        /// <returns>bool</returns>
+        internal bool ClearContentNode(HtmlNodeCollection nodesIndex, string attrValue)
+        {
+            foreach (var itemNode in nodesIndex)
+            {
+                var attrs = itemNode.Attributes;
+                foreach (var itemAttr in attrs)
+                {
+                    if (itemAttr.Value == attrValue)
+                    {
+                        itemNode.RemoveAllChildren();
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
