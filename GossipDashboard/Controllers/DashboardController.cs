10fa2931e8c17243059876b3504a9448e97aaecd
m.kabi <mahmoodkabi@gmail.com>
2018-10-27 21:42:32 +0330

    add dashboard front end

---
+++ GossipDashboard/Controllers/DashboardController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GossipDashboard.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Pages()
+        {
+            return View();
+        }
+
+
+        public ActionResult Posts()
+        {
+            return View();
+        }
+
+        public ActionResult Users()
+        {
+            return View();
+        }
+
+        
+
+    }
+}
